7689adc17c025ac892cf8d777da98621731898aa
jeengbe <jesper.engberg@gmx.at>
2021-02-02 08:09:40 +0100

    Fix Update statement

--- src/database.ts
+++ src/database.ts
@@ -2,15 +2,15 @@ import { db } from "./connection";
 import * as Discord from "discord.js";
 
 export const addServer = (server: Discord.Guild): void => {
-  db.query("INSERT INTO servers (ID, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = VALUES(name)", [server.id, server.name]);
+  db.query("INSERT INTO servers (ID, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = ?", [server.id, server.name, server.name]);
 };
 
 export const addUser = (user: Discord.GuildMember): void => {
-  db.query("INSERT INTO users (ID, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = VALUES(name)", [user.id, user.nickname || user.displayName]);
+  db.query("INSERT INTO users (ID, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = ?", [user.id, user.nickname || user.displayName, user.nickname || user.displayName]);
 };
 
 export const addChannel = (channel: Discord.Channel & { name: string }): void => {
-  db.query("INSERT INTO channels (ID, type, name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = VALUES(name)", [channel.id, channel.type, channel.name]);
+  db.query("INSERT INTO channels (ID, type, name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = ?", [channel.id, channel.type, channel.name, channel.name]);
 };
 
 export const voiceAction = (server: Discord.Snowflake, channel: Discord.Snowflake, user: Discord.Snowflake, join: boolean): void => {
